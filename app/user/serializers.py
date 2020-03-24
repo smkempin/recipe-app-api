1294d6257e52266910b6cd6dc3e6d71e45430531
smkempin <scott@precisionwre.com>
2020-03-23 22:52:44 -0500

    manage user endpoint

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -15,6 +15,17 @@ class UserSerializer(serializers.ModelSerializer):
         """create a new user with encrypted password and return it"""
         return get_user_model().objects.create_user(**validated_data)
 
+    def update(self, instance, validated_data):
+        """update a user, setting the password correctly and return int"""
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
+
 class AuthTokenSerializer(serializers.Serializer):
     """SERializer for the user auth object"""
     email = serializers.CharField()

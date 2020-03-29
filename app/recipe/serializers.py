980fc6ec64c37d5f31f619d9f76ad186e27aff32
smkempin <scott@precisionwre.com>
2020-03-29 10:21:38 -0500

    added custom recipe detail view

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -29,8 +29,6 @@ class RecipeSerializer(serializers.ModelSerializer):
         queryset=Tag.objects.all()
     )
 
-
-
     class Meta:
         model = Recipe
         fields = (
@@ -38,3 +36,8 @@ class RecipeSerializer(serializers.ModelSerializer):
             'price', 'link'
         )
         read_only_fields = ('id',)
+
+class RecipeDetailSerializer(RecipeSerializer):
+    """Serializer a recipe detail"""
+    ingredients = IngredientSerializer(many=True, read_only=True)
+    tags = TagSerializer(many=True, read_only=True)
